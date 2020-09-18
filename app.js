f0a672f0f1e74ac77965f3ce5f706f4a821bf1f4
plat0033 <plat0033@algonquinlive.com>
2020-09-18 12:51:55 -0400

    portfolioPage completed

--- app.js
+++ app.js
@@ -13,3 +13,15 @@ window.onscroll = function() {
     }
 
 }
+
+// let i = 0
+// const text = 'Creating websites that engage users.'
+// const speed = 50
+
+// window.onload = function typeWriter() {
+//     if( i < text.length ) {
+//         document.getElementById('typewriter').innerHTML += text.charAt(i);
+//         i++
+//         setTimeout(typeWriter,speed)
+//     }
+// }
